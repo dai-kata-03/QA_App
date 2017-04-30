26d110b039af3b37aed191fca4d805eb37834252
Daisuke <siq009@yahoo.ca>
2017-04-30 16:23:59 -0400

    Chapter6まで終了。

---
+++ app/src/main/java/jp/techacademy/katahara/daisuke/qa_app/Const.java
@@ -0,0 +1,13 @@
+package jp.techacademy.katahara.daisuke.qa_app;
+
+/**
+ * Created by Daisuke on 2017/04/29.
+ */
+
+public class Const {
+    public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するパス
+    public static final String AnswerPATH = "answers"; // Firebaseに解答を保存するパス
+
+    public static final String NameKEY = "name"; // Firebaseに表示名を保存する時の
+}
